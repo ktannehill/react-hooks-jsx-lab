594921a1d0d5fe7db6f0c218f18e5df42cdcc80c
Kat Tannehill <katetannehill@gmail.com>
2023-10-01 11:03:47 -0600

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <img src={ image } alt="I made this"/>
+    </div>
+  );
 }
 
 export default About;
